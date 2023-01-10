fe96ad46e4b5fedb431b43f2892be60f6d71ce01
Василий Бураев <buraevvasilii@gmail.com>
2023-01-10 12:00:00 +0300

    delite rudiment file

--- components/index.js
+++ components/index.js
@@ -3,6 +3,5 @@ import Hero from "./coms/homepage/hero";
 import HomeGrid from "./coms/homepage/HomeGrid";
 import HomeAbout from "./coms/homepage/HomeAbout";
 import Contact from "./coms/common/Contact";
-import HomeShowCase from "./coms/homepage/HomeShowCase";
 
-export { Layout, Hero, HomeGrid, HomeAbout, Contact, HomeShowCase };
+export { Layout, Hero, HomeGrid, HomeAbout, Contact };
